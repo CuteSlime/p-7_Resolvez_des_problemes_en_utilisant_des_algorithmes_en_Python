632a1ed68b24b494a14fda2e12f08cc89f77be5b
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2023-10-24 20:24:34 +0200

    add portfolio model

---
+++ Models/portfolio.py
@@ -0,0 +1,13 @@
+class Portfolio():
+    def __init__(self, **kwargs):
+        self.stock_list = kwargs.get("stock_list", [])
+        self.investment = kwargs.get("investment", 500)
+
+    def add_stock(self, stock: object):
+        self.stock_list.append(stock)
+
+    def calculate_total_gain(self):
+        total_gain = 0
+        for stock in self.stock_list:
+            total_gain += stock.gain
+        return total_gain
