f4902532cdce77812ee4369fa58ede7efc4f78e4
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2023-10-25 17:54:15 +0200

    update view

--- Views/base.py
+++ Views/base.py
@@ -1,3 +1,4 @@
+
 class View():
     def __init__(self):
         pass
@@ -5,5 +6,6 @@ class View():
     def stocks_to_buy_with_gain(self, portfolio):
         for stock in portfolio.stock_list:
             print(f"{stock.name} raporte : {stock.gain}")
-        print(f"{portfolio.investment} restant sur le compte")
-        print("Total Gain:", portfolio.calculate_total_gain())
+        print(f"montant total des actions :{500 - portfolio.investment}")
+        print("Gain total des actions apres 2 ans :",
+              portfolio.calculate_total_gain())
