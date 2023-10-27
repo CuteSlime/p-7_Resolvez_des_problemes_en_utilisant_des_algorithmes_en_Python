7a14db49c9223e620fccf65340b88f47d9d2a91c
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2023-10-27 14:00:44 +0200

    move data to top

--- bruteforce.py
+++ bruteforce.py
@@ -4,6 +4,30 @@ import time
 
 start_time = time.time() * 1000
 
+# data
+stocks = [
+    {"name": "Action-1", "price": 20, "profit": 5},
+    {"name": "Action-2", "price": 30, "profit": 10},
+    {"name": "Action-3", "price": 50, "profit": 15},
+    {"name": "Action-4", "price": 70, "profit": 20},
+    {"name": "Action-5", "price": 60, "profit": 17},
+    {"name": "Action-6", "price": 80, "profit": 25},
+    {"name": "Action-7", "price": 22, "profit": 7},
+    {"name": "Action-8", "price": 26, "profit": 11},
+    {"name": "Action-9", "price": 48, "profit": 13},
+    {"name": "Action-10", "price": 34, "profit": 27},
+    {"name": "Action-11", "price": 42, "profit": 17},
+    {"name": "Action-12", "price": 110, "profit": 9},
+    {"name": "Action-13", "price": 38, "profit": 23},
+    {"name": "Action-14", "price": 14, "profit": 1},
+    {"name": "Action-15", "price": 18, "profit": 3},
+    {"name": "Action-16", "price": 8, "profit": 8},
+    {"name": "Action-17", "price": 4, "profit": 12},
+    {"name": "Action-18", "price": 10, "profit": 14},
+    {"name": "Action-19", "price": 24, "profit": 21},
+    {"name": "Action-20", "price": 114, "profit": 18},
+]
+
 
 class Stock():
     def __init__(self, name, stock_price, percent_gain):
@@ -62,30 +86,6 @@ def best_portfolio(stocks_list: list):
 
     portfolio = sorted_portfolios[0]
     return portfolio
-
-
-stocks = [
-    {"name": "Action-1", "price": 20, "profit": 5},
-    {"name": "Action-2", "price": 30, "profit": 10},
-    {"name": "Action-3", "price": 50, "profit": 15},
-    {"name": "Action-4", "price": 70, "profit": 20},
-    {"name": "Action-5", "price": 60, "profit": 17},
-    {"name": "Action-6", "price": 80, "profit": 25},
-    {"name": "Action-7", "price": 22, "profit": 7},
-    {"name": "Action-8", "price": 26, "profit": 11},
-    {"name": "Action-9", "price": 48, "profit": 13},
-    {"name": "Action-10", "price": 34, "profit": 27},
-    {"name": "Action-11", "price": 42, "profit": 17},
-    {"name": "Action-12", "price": 110, "profit": 9},
-    {"name": "Action-13", "price": 38, "profit": 23},
-    {"name": "Action-14", "price": 14, "profit": 1},
-    {"name": "Action-15", "price": 18, "profit": 3},
-    {"name": "Action-16", "price": 8, "profit": 8},
-    {"name": "Action-17", "price": 4, "profit": 12},
-    {"name": "Action-18", "price": 10, "profit": 14},
-    {"name": "Action-19", "price": 24, "profit": 21},
-    {"name": "Action-20", "price": 114, "profit": 18},
-]
 
 
 stocks_list = []
