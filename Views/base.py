02e80e2f7efa3d0bdcd2b30e20553574ada8b439
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2023-10-27 14:52:41 +0200

    add round to view

--- Views/base.py
+++ Views/base.py
@@ -5,7 +5,7 @@ class View():
 
     def stocks_to_buy_with_gain(self, portfolio):
         for stock in portfolio.stock_list:
-            print(f"{stock.name} raporte : {stock.gain}")
-        print(f"montant total des actions :{500 - portfolio.investment}")
+            print(f"{stock.name} raporte : {stock.gain}€")
+        print(f"montant total des actions :{500 - portfolio.investment}€")
         print("Gain total des actions apres 2 ans :",
-              portfolio.calculate_total_gain())
+              round(portfolio.calculate_total_gain(), 2), "€")
