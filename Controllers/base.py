af6dfa8d35f81ca4f24aeb04e281935946dca860
Lydian Orlandini <8015348+CuteSlime@users.noreply.github.com>
2023-10-27 14:53:23 +0200

    update controller to reflect standalone script change

--- Controllers/base.py
+++ Controllers/base.py
@@ -1,31 +1,33 @@
-import csv
 import time
+import csv
+import itertools
 from Models.stock import Stock
 from Models.portfolio import Portfolio
 start_time = time.time() * 1000
 
 
+# data
 stocks = [
-    {"name": "Action-1", "stock_price": 20, "percent_gain": 5},
-    {"name": "Action-2", "stock_price": 30, "percent_gain": 10},
-    {"name": "Action-3", "stock_price": 50, "percent_gain": 15},
-    {"name": "Action-4", "stock_price": 70, "percent_gain": 20},
-    {"name": "Action-5", "stock_price": 60, "percent_gain": 17},
-    {"name": "Action-6", "stock_price": 80, "percent_gain": 25},
-    {"name": "Action-7", "stock_price": 22, "percent_gain": 7},
-    {"name": "Action-8", "stock_price": 26, "percent_gain": 11},
-    {"name": "Action-9", "stock_price": 48, "percent_gain": 13},
-    {"name": "Action-10", "stock_price": 34, "percent_gain": 27},
-    {"name": "Action-11", "stock_price": 42, "percent_gain": 17},
-    {"name": "Action-12", "stock_price": 110, "percent_gain": 9},
-    {"name": "Action-13", "stock_price": 38, "percent_gain": 23},
-    {"name": "Action-14", "stock_price": 14, "percent_gain": 1},
-    {"name": "Action-15", "stock_price": 18, "percent_gain": 3},
-    {"name": "Action-16", "stock_price": 8, "percent_gain": 8},
-    {"name": "Action-17", "stock_price": 4, "percent_gain": 12},
-    {"name": "Action-18", "stock_price": 10, "percent_gain": 14},
-    {"name": "Action-19", "stock_price": 24, "percent_gain": 21},
-    {"name": "Action-20", "stock_price": 114, "percent_gain": 18},
+    {"name": "Action-1", "price": 20, "profit": 5},
+    {"name": "Action-2", "price": 30, "profit": 10},
+    {"name": "Action-3", "price": 50, "profit": 15},
+    {"name": "Action-4", "price": 70, "profit": 20},
+    {"name": "Action-5", "price": 60, "profit": 17},
+    {"name": "Action-6", "price": 80, "profit": 25},
+    {"name": "Action-7", "price": 22, "profit": 7},
+    {"name": "Action-8", "price": 26, "profit": 11},
+    {"name": "Action-9", "price": 48, "profit": 13},
+    {"name": "Action-10", "price": 34, "profit": 27},
+    {"name": "Action-11", "price": 42, "profit": 17},
+    {"name": "Action-12", "price": 110, "profit": 9},
+    {"name": "Action-13", "price": 38, "profit": 23},
+    {"name": "Action-14", "price": 14, "profit": 1},
+    {"name": "Action-15", "price": 18, "profit": 3},
+    {"name": "Action-16", "price": 8, "profit": 8},
+    {"name": "Action-17", "price": 4, "profit": 12},
+    {"name": "Action-18", "price": 10, "profit": 14},
+    {"name": "Action-19", "price": 24, "profit": 21},
+    {"name": "Action-20", "price": 114, "profit": 18},
 ]
 
 
@@ -37,17 +39,9 @@ class Controller():
         with open(f'./Data/{filename}.csv', 'r', encoding="utf-8-sig", newline="") as stocks:
             reader = csv.DictReader(
                 stocks, dialect='excel', delimiter=',', quotechar='"')
-            stocks = []
-            for row in reader:
-                if float(row['price']) > 0:
-                    stocks.append(row)
-        stocks_list = []
-        for stock in stocks:
-            stocks_list.append(Stock(stock["name"],
-                                     float(stock["price"]),
-                                     float(stock["profit"])
-                                     ))
-        return stocks_list
+            stocks = [row for row in reader if float(row['price']) > 0]
+
+        return stocks
 
     def optimized_portfolio(self, stocks_list):
         sorted_stocks = sorted(stocks_list,
@@ -59,21 +53,54 @@ class Controller():
         for stock in sorted_stocks:
             if stock.stock_price <= portfolio.investment:
                 portfolio.add_stock(stock)
-                portfolio.investment -= stock.stock_price
 
         return portfolio
 
+    def create_portfolio_list(self, list_a):
+        max_total_gain = 0
+        for list_of_stocks in list_a:
+            portfolio = Portfolio()
+
+            if sum(stock.stock_price for stock in list_of_stocks) <= portfolio.investment:
+                for item in list_of_stocks:
+                    portfolio.add_stock(item)
+                total_gain = portfolio.calculate_total_gain()
+                if total_gain > max_total_gain:
+                    max_total_gain = total_gain
+                    yield portfolio
+
+    def bruteforce_portfolio(self, stocks_list: list):
+
+        i = 2
+        list_a = []
+        while i < len(stocks_list):
+            list_of_tulpe = itertools.combinations(stocks_list, i)
+            list_a.extend(list_of_tulpe)
+            i += 1
+
+        portfolios = self.create_portfolio_list(list_a)
+
+        sorted_portfolios = sorted(portfolios,
+                                   key=lambda portfolio: portfolio.calculate_total_gain(),
+                                   reverse=True
+                                   )
+
+        portfolio = sorted_portfolios[0]
+        return portfolio
+
     def run(self):
-        # filename = 'dataset1_Python+P7'
+
         stocks_list = []
+        filename = 'dataset1_Python+P7'
+        stocks = self.csv_converter(filename)
         for stock in stocks:
             stocks_list.append(Stock(stock["name"],
-                                     float(stock["stock_price"]),
-                                     float(stock["percent_gain"])
+                                     float(stock["price"]),
+                                     float(stock["profit"])
                                      ))
-        # stocks_list = self.csv_converter(filename)
 
         portfolio = self.optimized_portfolio(stocks_list)
+        # portfolio = self.bruteforce_portfolio(stocks_list)
 
         self.view.stocks_to_buy_with_gain(portfolio)
         print("--- %s ms ---" % ((time.time()*1000) - start_time))
